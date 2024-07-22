e222a20a9b43adfcd2fe5d9fa691e084ed9dbc0f
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-07-22 11:26:25 -0500

    Add quotes database

---
+++ database.py
@@ -0,0 +1,7 @@
+quotes = [
+    "Eres capaz de cosas asombrosas.",
+    "Cree en ti mismo y en todo lo que eres.",
+    "El éxito no es definitivo, el fracaso no es fatal: lo que cuenta es el coraje de continuar.",
+    "El único límite para nuestra realización del mañana serán nuestras dudas de hoy.",
+    "El futuro pertenece a aquellos que creen en la belleza de sus sueños."
+]
