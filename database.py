b919a4cf1e4e6e1fc3d1b4aa5dc7104928b42384
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-07-22 13:09:50 -0500

    Add quotes to the database (Colombian Lore)

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 quotes = [
-    "Eres capaz de cosas asombrosas.",
-    "Cree en ti mismo y en todo lo que eres.",
-    "El éxito no es definitivo, el fracaso no es fatal: lo que cuenta es el coraje de continuar.",
-    "El único límite para nuestra realización del mañana serán nuestras dudas de hoy.",
-    "El futuro pertenece a aquellos que creen en la belleza de sus sueños."
+    "Vos sabes el significado de la palabra tetranutra?",
+    "Usted sabe cual es la capital de Bangladesh?",
+    "Todo bien, todo bonito, solo nacional a morir.",
+    "Kelly pero que monda.",
+    "Péguelo socio, péguelo.",
 ]
